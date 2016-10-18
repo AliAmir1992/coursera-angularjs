821644ffafaef3fb4839161c5f7e30b90dcd6fa7
aliamir1992 <aliamirshamim@gmail.com>
2016-10-18 17:43:01 +0000

    changes in assignment : added additional functionality

--- assignment1/app.js
+++ assignment1/app.js
@@ -9,14 +9,32 @@
 
     function lunchItemsCount($scope) {
         $scope.itemsCSV = "";
+        $scope.changeColor = "red";
+        $scope.changeBorderColor = "";
         $scope.countItems = function () {
+            
             if ($scope.itemsCSV == "") {
                 $scope.lunch_status = "Please enter data first";
+                $scope.changeBorderColor = "borderRed";
+                
                 return;
             }
             var items = $scope.itemsCSV.split(',');
 
             $scope.lunch_status = items.length <= 3 ? "Enjoy!" : "Too much!";
+            
+            
+            if($scope.lunch_status == "Enjoy!" || $scope.lunch_status == "Too much!")
+                $scope.changeColor = "green";
+            else
+                $scope.changeColor = "red";
+                
+            if($scope.lunch_status == "Enjoy!" || $scope.lunch_status == "Too much!")
+                $scope.changeBorderColor = "borderGreen";
+            else
+                $scope.changeBorderColor = "borderRed";
+                
+            
         };
     }
 })();
